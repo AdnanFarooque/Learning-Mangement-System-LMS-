0fee7a2511766efa1d1d175890b72f474bdc57ec
AdnanFarooque <adnan.dipta89@gmail.com>
2022-04-09 17:50:49 +0600

    Updated

--- lms/settings.py
+++ lms/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = ')7gno0)u%l2-(b3-1u3h)-@nra728h*2i-vh()=(3yym8&e2e4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
-DISABLE_COLLECTSTATIC=1
+heroku config:set DISABLE_COLLECTSTATIC=1
     
 ALLOWED_HOSTS = ["/https://lms-term-project.herokuapp.com/", "127.0.0.1"]
 
